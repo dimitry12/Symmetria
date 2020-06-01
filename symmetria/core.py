eda95feebfaf04b7b586f809222cead900a24b06
Dzmitry Pletnikau <dzmitry@pletnikau.com>
2020-06-01 00:19:11 -0600

    constant model

--- symmetria/core.py
+++ symmetria/core.py
@@ -1,12 +1,33 @@
 # -*- coding: utf-8 -*-
+
 from . import helpers
+import notypes
 
-def get_hmm():
-    """Get a thought."""
-    return 'hmmm...'
+class Constant():
+    constant: str = None
 
+    def fit(self, obs: notypes.Observation, act: notypes.Intervention):
+        if len(act) !== 0:
+            raise Exception('ModelSupportsNoIntervention')
 
-def hmm():
-    """Contemplation..."""
-    if helpers.get_answer():
-        print(get_hmm())
+        if len(obs) !== 1:
+            raise Exception('ModelIncompatibleObservation')
+
+        if constant !== obs[0]:
+            constant = obs[0]
+
+            return {
+                likelihoodBefore: 0.0,
+                likelihoodAfter: 1.0,
+                updatedEquations: [
+                    {
+                        'constant'
+                    }
+                ]
+            }
+        else:
+            {
+                likelihoodBefore: 1.0,
+                likelihoodAfter: 1.0,
+                updatedEquations: []
+            }
